1be87b3618b079691792389219bb4985de92424d
Elcin <elcinertugrul@thorntontomasetti.com>
2014-10-20 15:34:28 -0400

    Prototype Kick Off

---
+++ src/DynamoSAP/DynamoSAP/Structure/DS_Frame.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DynamoSAP.Structure
+{
+    public class DS_Frame
+    {
+        public string Label
+        {
+            get { return "hello"; }
+        }
+
+        public static string CreateFrmFromCrv(string hi)
+        {
+            if (hi == "hello")
+            {
+                return "hello";
+            }
+            else { return "hey"; }
+        
+        }
+    }
+}
---
+++ src/DynamoSAP/DynamoSAP/Utilities/SElement.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Autodesk.DesignScript.Runtime;
+
+namespace DynamoSAP.Utilities
+{
+    [IsVisibleInDynamoLibrary(false)]
+    public class SElement
+    {
+        public string GUID { get; set; }
+        public string Label { get; set; }
+        public SType Type;
+
+    }
+
+    public class SType
+    { 
+        // Write enum
+        // Frame, Area, Link, Cable etc
+    
+    }
+}
