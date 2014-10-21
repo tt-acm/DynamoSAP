98f4d647aa201e94f0079b5750e28cb0f9dfd862
Elcin <elcinertugrul@thorntontomasetti.com>
2014-10-21 16:19:05 -0400

    Prototyping!  got something working..

---
+++ src/DynamoSAP/DynamoSAP/Structure/Frame.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using DynamoSAP.Utilities;
+using Autodesk.DesignScript.Geometry;
+
+namespace DynamoSAP.Structure
+{
+    public class Frame
+    {
+        // FIELDS
+        // Curve  class that holds nodes ! (vertices)
+        private Line BaseCrv { get; set; }
+
+        //matprop
+        private string MatProp { get; set; }
+        //sectionprop
+        private string SecProp { get; set; }
+        //justification - follow SAP enum for InsertionPoint set default 5
+        private int Just { get; set; } 
+        //rotation
+        private double Angle { get; set; }
+
+
+        public static double Rotation (Frame f)
+        {
+            return f.Angle;
+        }
+
+        public static Line BaseCurve (Frame f)
+        {
+            return f.BaseCrv;
+        }
+
+        public static string getType(object o)
+        {
+            return o.GetType().ToString();
+        }
+        
+
+        // STATIC CONSTRUCTORS
+
+        public static Frame FromCurve(Line Line, string matProp, string secProp, int just, double angle)
+        {
+            return new Frame(Line,matProp, secProp, just, angle);
+        }
+
+        
+        private Frame(){}
+        private Frame(Line line,string matProp, string secProp, int just, double angle)
+        {
+            BaseCrv = line;
+            Angle = angle;
+            MatProp = matProp;
+            SecProp= secProp;
+            Just = just;
+        }
+
+    }
+}
