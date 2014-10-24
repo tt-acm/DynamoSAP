6b9243be026259465643c3d52c3a4a970d39f432
eertugrul <eertugrul@thorntontomasetti.com>
2014-10-24 17:53:58 -0400

    Load Pattern Type Dropdown Node

---
+++ src/DynamoSAP_UI/DynamoSAPDropDowns.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Xml;
+using DSCoreNodesUI;
+using Dynamo.Models;
+using SAP2000v16;
+
+namespace DynamoSAP_UI
+{
+    [NodeName("LoadPatternTypeDropDown")]
+    [NodeCategory("DynamoSAP.DynamoSAP.Analysis.LoadPattern")]
+    [NodeDescription("Select Load Pattern from the dropdown")]
+    [IsDesignScriptCompatible]
+    public class LoadPatternTypeDropDown: EnumAsInt<eLoadPatternType>
+    {
+        public LoadPatternTypeDropDown(WorkspaceModel workspace) : base(workspace) { }
+    }
+}
