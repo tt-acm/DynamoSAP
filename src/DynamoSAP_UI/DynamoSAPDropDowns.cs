29256974a52871daeb812c46ac5e9037d4df5b50
eertugrul <eertugrul@thorntontomasetti.com>
2014-10-27 11:39:55 -0400

    Load Case Types dropdown Added

--- src/DynamoSAP_UI/DynamoSAPDropDowns.cs
+++ src/DynamoSAP_UI/DynamoSAPDropDowns.cs
@@ -18,6 +18,30 @@ namespace DynamoSAP_UI
         public LoadPatternTypeDropDown(WorkspaceModel workspace) : base(workspace) { }
     }
 
+    [NodeName("LoadCaseTypeDropDown")]
+    [NodeCategory("DynamoSAP.DynamoSAP.Analysis.LoadCase")]
+    [NodeDescription("Select Load Case to use with Set Load Case node")]
+    [IsDesignScriptCompatible]
+    public class LoadCaseTypeDropDown : EnumAsString<eLoadCaseType>
+    {
+        //CASE_LINEAR_STATIC = 1
+        //CASE_NONLINEAR_STATIC = 2
+        //CASE_MODAL = 3
+        //CASE_RESPONSE_SPECTRUM = 4
+        //CASE_LINEAR_HISTORY = 5  (Modal Time History)
+        //CASE_NONLINEAR_HISTORY = 6  (Modal Time History)
+        //CASE_LINEAR_DYNAMIC = 7  (Direct Integration Time History)
+        //CASE_NONLINEAR_DYNAMIC = 8  (Direct Integration Time History)
+        //CASE_MOVING_LOAD = 9
+        //CASE_BUCKLING = 10
+        //CASE_STEADY_STATE = 11
+        //CASE_POWER_SPECTRAL_DENSITY = 12
+        //CASE_LINEAR_STATIC_MULTISTEP = 13
+        //CASE_HYPERSTATIC = 14
+
+        public LoadCaseTypeDropDown(WorkspaceModel workspace) : base(workspace) { }
+    }
+
     [NodeName("JustificationDropDown")]
     [NodeCategory("DynamoSAP.DynamoSAP.Structure.Frame")]
     [NodeDescription("Select Justification to use with Create Frame nodes")]
@@ -25,9 +49,7 @@ namespace DynamoSAP_UI
     public class JustificationTypeDropdown : EnumAsString<Justification>
     {
         public JustificationTypeDropdown(WorkspaceModel workspace) : base(workspace) { }
-    
     }
-
 
     public enum Justification :int // Dynamo change the order, ans starts at 0
     {
