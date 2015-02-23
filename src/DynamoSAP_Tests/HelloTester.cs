1641517e780039aa4d3e114da8604e75e2a0b7d9
Dan Reynolds <DReynolds@ThorntonTomasetti.com>
2015-02-23 17:56:02 -0500

    try to set up system tests

---
+++ src/DynamoSAP_Tests/HelloTester.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SystemTestServices;
+using NUnit.Framework;
+
+namespace DynamoSAP_Tests
+{
+    [TestFixture]
+    public class HelloTester : SystemTestBase
+    {   
+        [Test]
+        public void ian() 
+        {
+            AssertNoDummyNodes();
+        }
+    }
+}
