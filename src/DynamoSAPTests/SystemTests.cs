2709a400c79aa75712f35c570afb231afda9e8a1
Elcin <eertugrul@thorntontomasetti.com>
2015-06-22 15:11:23 -0400

    Create Dynamo Test Project + Parameterize the dlls

---
+++ src/DynamoSAPTests/SystemTests.cs
@@ -0,0 +1,45 @@
+﻿using System.IO;
+using System.Reflection;
+using NUnit.Framework;
+using RevitTestServices;
+using RTF.Framework;
+using Autodesk.Revit.DB;
+using RevitServices.Persistence;
+
+
+namespace DynamoSAPTests
+{
+    [TestFixture]
+    public class SystemTestExample : RevitSystemTestBase
+    {
+        [SetUp]
+        public void Setup()
+        {
+            // Set the working directory. This will allow you to use the OpenAndRunDynamoDefinition method,
+            // specifying a relative path to the .dyn file you want to test.
+
+            var asmDirectory = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            workingDirectory = Path.GetFullPath(Path.Combine(asmDirectory,
+                @"..\..\..\packages\DynamoSAP\extra"));
+        }
+
+        /// <summary>
+        /// No Need for Revit model !!! 
+        /// </summary>
+        [Test, TestModel(@".\TestModel.rvt")]
+        public void DefineStructure()
+        { 
+            //Open and Run the sample file
+            OpenAndRunDynamoDefinition(@".\Sample_1a_SpaceStructure.dyn");
+
+            // Test Logic is here
+            //if (false)
+            //{
+            //    Assert.Fail();
+            //}
+
+            //if we got here, nothing failed.
+            Assert.Pass();
+        }
+    }
+}
