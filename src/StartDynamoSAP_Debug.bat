8e6022baf3fc623b763e75c3e7326d7a625615a5
Ana <agarciapuyol@thorntontomasetti.com>
2016-02-26 11:56:29 -0500

    references path updated

--- src/StartDynamoSAP_Debug.bat
+++ src/StartDynamoSAP_Debug.bat
@@ -1,4 +1,4 @@
-set DYNAMO_API=C:\Program Files\Dynamo 0.8
-set REVIT_API=C:\Program Files\Autodesk\Revit 2014
-set REVIT_VERSION=Revit_2014
+set DYNAMO_API=C:\Program Files\Dynamo 0.9
+set REVIT_API=C:\Program Files\Autodesk\Revit 2015
+set REVIT_VERSION=Revit_2015
 "C:\Program Files (x86)\Microsoft Visual Studio 12.0\Common7\IDE\devenv.exe" "DynamoSAP.sln"
